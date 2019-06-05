52d7ed314bc551a2f1f1a616599b93bb72354ea3
yochidros <mm9.movement.trb@gmail.com>
2019-06-05 12:15:54 +0900

    Initial Commit

---
+++ TutorialSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TutorialSwiftUI
+//
+//  Created by yochidros on 2019/06/05.
+//  Copyright © 2019 yochidros. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
