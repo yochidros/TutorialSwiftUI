40d921935cda51ab171d5a3028155a345f761ca5
yochidros <mm9.movement.trb@gmail.com>
2019-06-15 17:05:24 +0900

    feat: first tutorial .

--- TutorialSwiftUI/ContentView.swift
+++ TutorialSwiftUI/ContentView.swift
@@ -10,7 +10,31 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -100)
+                .padding(.bottom, -100)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
     }
 }
 
---
+++ TutorialSwiftUI/Views/MapView.swift
@@ -0,0 +1,34 @@
+//
+//  MapView.swift
+//  TutorialSwiftUI
+//
+//  Created by yochidros on 2019/06/11.
+//  Copyright © 2019 yochidros. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView : UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286,
+            longitude: -116.166868
+        )
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
